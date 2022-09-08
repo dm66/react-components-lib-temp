ba54c55d571a18e70ca0e35acf13bdd71dbc718c
”wenmq“ <wenmq@akulaku.com>
2022-09-08 17:18:49 +0800

    feat: 初始化react组件库测试

---
+++ dist/esm/Foo/index.js
@@ -0,0 +1,6 @@
+// src/Foo/index.tsx
+import React from "react";
+var Foo_default = ({ title }) => /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("h1", null, title), /* @__PURE__ */ React.createElement("h1", null, title));
+export {
+  Foo_default as default
+};
---
+++ dist/esm/index.js
@@ -0,0 +1,7 @@
+// src/index.ts
+import { default as default2 } from "./Foo";
+import { default as default3 } from "./Button";
+export {
+  default3 as Button,
+  default2 as Foo
+};
