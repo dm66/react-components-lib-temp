72035a50bfee3623ba39dc2ed662cca6f4f1edac
”wenmq“ <wenmq@akulaku.com>
2022-09-15 16:24:13 +0800

    feat: 修改gitignore

--- .umirc.ts
+++ .umirc.ts
@@ -1,11 +1,10 @@
 // more config: https://d.umijs.org/config
-
 import { defineConfig } from 'dumi';
 
 const BaseUrl = '/react-components-lib-temp'; // 访问的仓库路径
 
 export default defineConfig({
-  title: 'test-dumi-lib',
+  title: 'test-dumi-father-lib',
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
@@ -15,6 +14,7 @@ export default defineConfig({
   publicPath: BaseUrl + '/', // 打包文件时，引入地址生成 publicPath/xxx.js
   exportStatic: {}, // 对每隔路由输出html
   dynamicImport: {}, // 动态导入
+  alias: {},
 
   hash: true, //加hash配置，清除缓存
   manifest: {
